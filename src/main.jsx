7c0fc419e28dd6efdd1845de652df1409c87bc73
kamzak <58105737+kamzak@users.noreply.github.com>
2023-08-01 20:42:57 +0200

    Remove react query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 
 const queryClient = new QueryClient();
 
@@ -14,7 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <App />
-        <ReactQueryDevtools />
       </Provider>
     </QueryClientProvider>
   </>
