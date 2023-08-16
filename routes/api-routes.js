ec3957c8b33dc738f641d30b4e4fc35e7a67b29e
kamzak <58105737+kamzak@users.noreply.github.com>
2023-08-16 16:21:03 +0200

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://nodejs-blog-k7vl.vercel.app";
 
 export const API = {
   posts: `https://jsonplaceholder.typicode.com/posts`,
@@ -6,8 +6,9 @@ export const API = {
 
 export const NodeJsAPI = {
   posts: ({ sort, searchValue, prevOffset = 0 } = {}) =>
-    `${baseUrl}/posts${sort ? `?sort=${sort}` : "?sort=ascending"}${searchValue ? `&title=${searchValue}` : ""
+    `${baseUrl}/posts${sort ? `?sort=${sort}` : "?sort=ascending"}${
+      searchValue ? `&title=${searchValue}` : ""
     }${`&offset=${prevOffset}`}`,
   posts_trending: `${baseUrl}/posts?trending=true`,
-  posts_test: "http://localhost:3000/posts",
+  posts_test: `${baseUrl}/posts`,
 };
